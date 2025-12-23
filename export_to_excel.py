ca7a66d21e439d13d37e37e09a2195bd6c265b7e
realmbs <119774290+realmbs@users.noreply.github.com>
2025-12-23 12:31:27 -0600

    restart: initial commit

--- export_to_excel.py
+++
@@ -1,4 +0,0 @@
-# takes contacts_all.csv, contacts_high_quality.csv and exports them to excel using following template
-'''
-Owner	Organization Type	Organization Name	State	First Name	Last Name	Title / Role	Email Address	Contact Source	Follow-Up Status	Date 1st Email Sent	Date 2nd Follow-Up Sent	Date 3rd Follow-Up Sent	Date 4th Follow-Up Sent	Response Status?	Demo Date	Notes
-'''
